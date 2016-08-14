48e9a793db5b9a6755459cc963d4e92ad7e4c34c
Klaine <xzy694487445@qq.com>
2016-08-14 16:19:07 +0800

    fix picker ~

--- app/src/main/java/com/example/xzy/farm/SettingsActivity.java
+++ app/src/main/java/com/example/xzy/farm/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.xzy.farm;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,12 +8,11 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.settings);
 //        ImageView ima = (ImageView)findViewById(R.id.settingimage);
 //        ima.setOnClickListener(new View.OnClickListener() {
